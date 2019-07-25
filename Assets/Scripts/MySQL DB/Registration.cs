56154c2e5d215207cf06f41dc85c280e9a7add52
dj5052 <jayaward_im15018@stu.kln.ac.lk>
2019-07-25 14:46:43 +0530

    this is the first commit

---
+++ Assets/Scripts/MySQL DB/Registration.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Registration : MonoBehaviour
+{
+    public InputField nameField;
+    public InputField passwordField;
+
+    public Button submitButton;
+
+    
+
+}
