8c58914e81e25b713bafa899fe7d3c2b713bf8d9
dj5052 <jayaward_im15018@stu.kln.ac.lk>
2019-08-01 13:16:30 +0530

    added loginscreen UI

--- Assets/Scripts/MySQL DB/Registration.cs
+++ Assets/Scripts/MySQL DB/Registration.cs
@@ -10,6 +10,33 @@ public class Registration : MonoBehaviour
 
     public Button submitButton;
 
-    
+    public void CallRegister()
+    {
+        StartCoroutine(Register());
+    }
+
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name" , nameField.text);
+        form.AddField("password" , passwordField.text);
+        WWW www = new WWW("http://localhost/sqlconnect/register.php");
+        yield return www;
+        if(www.text == "0")
+        {
+            Debug.Log("user created successfully");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.Log("user creation failed. Error #"+ www.text);
+        }
+
+    }
+
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+    }
 
 }
